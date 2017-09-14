c329fbc52e36f027474ad44406de62eee24c8b42
Mat Booth <mat.booth@redhat.com>
2017-09-14 10:57:42 +0100

    Handle python bytecompiling, better handle name macros, add classpath substitutions and toggle some bconds

--- spec2scl/transformers/dts.py
+++ spec2scl/transformers/dts.py
@@ -15,18 +15,38 @@ class DTSTransformer(transformer.Transformer):
         scl_init = '%{{?scl:%scl_package {0}}}\n%{{!?scl:%global pkg_name %{{name}}}}\n%{{?java_common_find_provides_and_requires}}\n'.format(self.get_original_name(original_spec))
         return '{0}\n%global baserelease 0\n\n{1}'.format(scl_init, text)
 
+    @matches(r'brp-python-bytecompile', one_line=True, sections=['%header'])
+    def python_byte_compiling(self, original_spec, pattern, text):
+        return text.replace('brp-python-bytecompile', 'brp-scl-python-bytecompile', 1)
+
     @matches(r'^Release:', one_line=True, sections=settings.METAINFO_SECTIONS)
     def insert_baserelease(self, original_spec, pattern, text):
         return text.replace('%{?dist}', '.%{baserelease}%{?dist}', 1)
 
+    @matches(r'^%bcond_', one_line=True, sections=['%header'])
+    def toggle_bconds(self, original_spec, pattern, text):
+        bconds = ['memoryfilesystem','groovy','providers','subclipse']
+        bcond = text.split()[1]
+        if bcond in bconds:
+            return '%bcond_with {0}'.format(bcond)
+        return text
+
     @matches(r'^', one_line=False, sections=settings.RUNTIME_SECTIONS)
     def wrap_section_body(self, original_spec, pattern, text):
         head = text.splitlines(True)[0:1]
-        head.append('%{?scl:scl enable %{scl_maven} %{scl} - << "EOF"}\n')
+        head.append('%{?scl:scl enable %{scl_maven} %{scl} - << "EOFSCL"}\n')
         head.append('set -e -x\n')
         tail = text.splitlines(True)[1:]
-        tail.append('\n%{?scl:EOF}\n')
+        tail.append('\n%{?scl:EOFSCL}\n')
         return ''.join(head + tail)
+
+    @matches(r'^', one_line=True, sections=settings.RUNTIME_SECTIONS)
+    def build_classpath_substitutions(self, original_spec, pattern, text):
+        classpath_transforms = {"objectweb-asm/asm":"objectweb-asm5/asm-5", "objectweb-asm/asm-commons":"objectweb-asm5/asm-commons-5", "objectweb-asm/asm-util":"objectweb-asm5/asm-util-5"}
+        new_text = text
+        for key in classpath_transforms:
+            new_text = re.sub(r'\b%s\b' % key, classpath_transforms[key], new_text)
+        return new_text
 
     @matches(r'^%license', one_line=True, sections=['%files'])
     def eliminate_license_macro(self, original_spec, pattern, text):
@@ -68,11 +88,15 @@ class DTSTransformer(transformer.Transformer):
             groupdict = matchobj.groupdict('')
 
             # these deps are simply stripped
-            scl_ignored_deps = ['java-headless', 'java-devel']
+            scl_ignored_deps = ['java-headless', 'java-devel', 'python3-autopep8', 'python3-pep8', 'python3-pylint', 'python3-django', 'python3-ipython-console', 'python3-rpm-macros', 'maven-checkstyle-plugin', 'vagrant']
             # these are high priority maven deps (they are considered ahead of java-common deps)
             scl_hi_pri_maven_deps = ['javapackages-local', 'maven-local', 'ivy-local']
             # these deps have a different name to Fedora and must be transformed
-            scl_dep_transforms = { 'antlr':'antlr-tool', 'objectweb-asm':'objectweb-asm5', 'mvn(org.ow2.asm:asm-all)':'mvn(org.ow2.asm:asm-all:5)', 'mvn(org.ow2.asm:asm)':'mvn(org.ow2.asm:asm:5)', 'mvn(org.ow2.asm:asm-analysis)':'mvn(org.ow2.asm:asm-analysis:5)', 'mvn(org.ow2.asm:asm-commons)':'mvn(org.ow2.asm:asm-commons:5)', 'mvn(org.ow2.asm:asm-tree)':'mvn(org.ow2.asm:asm-tree:5)', 'mvn(org.ow2.asm:asm-util)':'mvn(org.ow2.asm:asm-util:5)', 'mvn(org.ow2.asm:asm-xml)':'mvn(org.ow2.asm:asm-xml:5)', 'easymock':'easymock2', 'mvn(org.easymock:easymock)':'mvn(org.easymock:easymock:2.4)'}
+            scl_dep_transforms = {'maven-lib':'maven', 'python3':'python', 'antlr':'antlr-tool', 'javax.mail':'javamail', 'batik-css':'batik', 'hamcrest-core':'hamcrest', 'lucene3':'lucene5', 'log4j12':'log4j',
+                    'mvn(cglib:cglib)':'mvn(cglib:cglib:3)', 'mvn(cglib:cglib-nodep)':'mvn(cglib:cglib-nodep:3)',
+                    'objectweb-asm':'objectweb-asm5', 'mvn(org.ow2.asm:asm-all)':'mvn(org.ow2.asm:asm-all:5)', 'mvn(org.ow2.asm:asm)':'mvn(org.ow2.asm:asm:5)', 'mvn(org.ow2.asm:asm-analysis)':'mvn(org.ow2.asm:asm-analysis:5)', 'mvn(org.ow2.asm:asm-commons)':'mvn(org.ow2.asm:asm-commons:5)', 'mvn(org.ow2.asm:asm-tree)':'mvn(org.ow2.asm:asm-tree:5)', 'mvn(org.ow2.asm:asm-util)':'mvn(org.ow2.asm:asm-util:5)', 'mvn(org.ow2.asm:asm-xml)':'mvn(org.ow2.asm:asm-xml:5)',
+                    'mvn(org.apache-extras.beanshell:bsh)':'mvn(org.beanshell:bsh)',
+                    'easymock':'easymock2', 'mvn(org.easymock:easymock)':'mvn(org.easymock:easymock:2.4)'}
             # collection provides lists
             scl_deps = self.options['scl_deps']
             scl_deps_maven = self.options['scl_deps_maven']
@@ -106,5 +130,14 @@ class DTSTransformer(transformer.Transformer):
             return tag + new_dep
         else:
             return ''
+
+    # matches name macros, but carefully avoiding messing with desktop files and their icons
+    @matches(r'(?<!Icon=)%{name}(?!\.desktop|\.png)', sections=settings.SPECFILE_SECTIONS)
+    def better_handle_name_macro(self, original_spec, pattern, text):
+        # instances of name macro in these tags should be left alone because they are
+        # intentionally referring to the name of the rpm we are currently processing
+        if text.startswith(('Obsoletes:', 'Provides:', 'Requires:', 'BuildRequires:', 'Recommends:')):
+            return text
+        return pattern.sub(r'%{pkg_name}', text)
 
 
